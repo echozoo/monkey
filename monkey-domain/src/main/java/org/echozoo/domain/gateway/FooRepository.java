840e95a75ca0ebf5e7e1c352cee486d644d3671e
dujf <dujf54732@gmail.com>
2021-12-08 20:02:47 +0800

    feat: init project

--- monkey-domain/src/main/java/org/echozoo/domain/gateway/FooRepository.java
+++ monkey-domain/src/main/java/org/echozoo/domain/gateway/FooRepository.java
@@ -2,6 +2,11 @@ package org.echozoo.domain.gateway;
 
 import org.echozoo.domain.entity.Foo;
 
+/**
+ * 数据仓促
+ *
+ * @author dujf
+ */
 public interface FooRepository {
 
     Boolean create(Foo foo);
